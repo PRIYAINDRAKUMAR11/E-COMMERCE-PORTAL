a4194b310e2551e3eebb3ede4c170cb8ad32bebf
PRIYAINDRAKUMAR11 <priyaindrakumar11@gmail.com>
2018-01-22 12:51:19 +0530

    my backup

---
+++ project_backend/src/main/java/com/model/Category.java
@@ -0,0 +1,48 @@
+
+package com.model;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table
+
+public class Category implements Serializable
+{
+
+	private static final long serialVersionUID =1L;
+	
+	@Id
+	/*@GeneratedValue(strategy=GenerationType.AUTO)*/
+	private int catId;
+	private String catName;
+	public int getCatId() {
+		return catId;
+	}
+	public void setCatId(int catId) {
+		this.catId = catId;
+	}
+	public String getCatName() {
+		return catName;
+	}
+	public void setCatName(String catName) {
+		this.catName = catName;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
+	
+
+	
+
+	
+	
+	
+
